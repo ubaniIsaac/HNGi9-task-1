162351bca82c5baf9a6cdba07218c646ff516a58
Isaac Ubani <isaacchimdi@gmail.com>
2022-11-03 20:30:38 +0100

    added stage 2 task- Post req to make calculation

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 
 const app = express()
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({
     origin: '*'
@@ -13,12 +13,29 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.json({
-        slackUsername: "isaacchimdi",
+        slackUsername: "i-ce-u",
         backend: true,
         age: 23,
         bio: "A backend developer that enjoys building cool things"
     })
 });
+app.post('/', (req, res) => {
+    const { operation_type, x, y } = req.body;
+    switch (operation_type) {
+        case 'addition': result = x + y
+            break;
+        case 'subtraction': result = x - y
+            break;
+        case 'multiplication': result = x * y
+            break;
+        default: result = "Invalid Operation type"
+    }
+    res.send({
+        slackUsername: "i-ce-u",
+        operation_type: operation_type,
+        result: result
+    })
+})
 
 
 app.set('port', PORT)
